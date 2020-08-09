94820d23112974391ced02b4db5972ebcc7dcab7
Deeptikanta Panda <pandde@kredifi.net>
2020-08-09 13:50:02 +0530

    Updates

---
+++ precache-manifest.24363c94988f222d86d0eccc798c95ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53bcf970052ccb55b3970e050308dea",
+    "url": "/outlook/index.html"
+  },
+  {
+    "revision": "bcebc941dddeaa2f4946",
+    "url": "/outlook/static/css/main.eac6241f.chunk.css"
+  },
+  {
+    "revision": "9819936d9fdfadc7a3f4",
+    "url": "/outlook/static/js/2.991d0407.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/outlook/static/js/2.991d0407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcebc941dddeaa2f4946",
+    "url": "/outlook/static/js/main.8e1fd9d3.chunk.js"
+  },
+  {
+    "revision": "8ab12c4f194f89c1b1aa",
+    "url": "/outlook/static/js/runtime-main.eef05079.js"
+  }
+]);
